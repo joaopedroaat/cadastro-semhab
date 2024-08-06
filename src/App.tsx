0cb3a068ad015c35c89da7f5b18019c3a2e59457
joaopedroaat <joaopedroaat@outlook.com>
2024-08-06 18:33:09 -0300

    Create name field

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,71 @@
-import { Button } from "./components/ui/button";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z.object({
+  name: z.string().min(3, {
+    message: "Nome deve conter no mínimo 3 caracteres",
+  }),
+});
 
 export function App() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   return (
     <>
       <header className="flex flex-col items-center justify-center">
         <h1 className="font-bold text-xl">SEMHAB</h1>
         <h2>Formulário de Cadastro</h2>
       </header>
-      <main className="h-screen w-full flex items-center justify-center">
-        <Button>Hello World!</Button>
+
+      <main className="mt-8">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome completo (sem abreviações)</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Nome completo do responsável familiar
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="mt-2" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
       </main>
     </>
   );
