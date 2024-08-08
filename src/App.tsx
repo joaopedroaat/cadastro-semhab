5bfba2054682f757834707af6c666f6d78cf3f23
joaopedroaat <joaopedroaat@outlook.com>
2024-08-07 22:33:13 -0300

    Remove unused lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./components/ui/select";
+import { Form } from "@/components/ui/form";
 import { useContext } from "react";
-import { IBGEContext } from "./contexts/IBGEContext";
 import { FormContext } from "./contexts/FormContext";
 import { NameField } from "./components/fields/NameField";
 import { SexField } from "./components/fields/SexField";
 import { NationalityField } from "./components/fields/NationalityField";
 
 export function App() {
-  const { states } = useContext(IBGEContext);
-
   const { form } = useContext(FormContext);
 
   function onSubmit(data: any) {
