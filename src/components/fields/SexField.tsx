b8eb29286c08b0d987ed879165182088654e4872
João Pedro <joaopedroaat@outlook.com>
2024-09-06 05:02:48 -0300

    Change SexField from select to radio

--- src/components/fields/SexField.tsx
+++ src/components/fields/SexField.tsx
@@ -1,20 +1,13 @@
 import { useContext } from "react";
 import {
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "../ui/form";
 import { FormContext } from "@/contexts/FormContext";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 
 export function SexField() {
   const { form } = useContext(FormContext);
@@ -23,20 +16,32 @@ export function SexField() {
       control={form.control}
       name="sex"
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>Sexo</FormLabel>
-          <Select onValueChange={field.onChange} defaultValue={field.value}>
-            <FormControl>
-              <SelectTrigger>
-                <SelectValue placeholder="Selecione o sexo" />
-              </SelectTrigger>
-            </FormControl>
-            <SelectContent>
-              <SelectItem value="male">Masculino</SelectItem>
-              <SelectItem value="female">Feminino</SelectItem>
-            </SelectContent>
-          </Select>
-          <FormDescription>Sexo do responsável familiar</FormDescription>
+        <FormItem className="space-y-3">
+          <FormLabel>Estado Civil</FormLabel>
+          <FormControl>
+            <RadioGroup
+              onValueChange={field.onChange}
+              defaultValue={field.value}
+              className="flex flex-col space-y-1"
+            >
+              <FormItem className="flex items-center space-x-3 space-y-0">
+                <FormControl>
+                  <RadioGroupItem value="male" />
+                </FormControl>
+                <FormLabel className="font-normal">
+                  Masculino
+                </FormLabel>
+              </FormItem>
+              <FormItem className="flex items-center space-x-3 space-y-0">
+                <FormControl>
+                  <RadioGroupItem value="female" />
+                </FormControl>
+                <FormLabel className="font-normal">
+                  Feminino
+                </FormLabel>
+              </FormItem>
+            </RadioGroup>
+          </FormControl>
           <FormMessage />
         </FormItem>
       )}
